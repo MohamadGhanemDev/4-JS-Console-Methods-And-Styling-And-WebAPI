3b64521ebd9ada4a356ceba78219da2895882c37
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-14 21:31:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+console.log("log");
+console.error("Error!!!!!");
+
+console.table(["Mhmd" , "Elie" , "Elzero"]);
+
+// directive c% not working on firefox
+console.log("c%Hello  c%World! World!" , "color: red; font-size: 40px" , "color: blue; font-size: 60px");
+
+// console is not for javascript console is for web api application programming interface 
+// so js uses api to execute , so console is from web api
